7badee0c469fc348bdf122c3082f2dcb07957fd0
marwanBenz <benzsram@gmail.com>
2024-10-15 17:59:55 +0100

    Added assurance page sections

--- resources/views/frontend/sp/assurance.blade.php
+++ resources/views/frontend/sp/assurance.blade.php
@@ -1,6 +1,175 @@
 <x-sp.head title="{{ setting('site.title') }}" />
 
+<!-- Services contact form -->
+<div class="section">
+    <div class="base-container w-container">
+        <div data-w-id="502c5dca-196a-0aae-1f67-5b18c9a90237" class="contacts-2-wrapper" style="align-items: flex-start !important">
+            <div class="contacts-2-content-wrapper">
+                <div class="image-box">
+                    <img src="assets/images/contact-image.png" alt="Contact image">
+                </div>
+                <div class="contacts-title">
+                    <h2 class="in-section-title">SEGURO</h2>
+                </div>
+                <div class="contacts-2-content">
+                    <p>En Immiworld, Nos comprometemos a ofrecer el mejor precio para nuestros servicios de seguro obligatorio para extranjeros en España. Entendemos que este servicio es esencial para los solicitantes de visado.</p>
+                </div>
+            </div>
+            <!-- Contact Form -->
+            <div class="contacts-2-form-wrapper">
+                <div class="form-block-contacts w-form" style="padding-top:40px">
+                    <form id="email-form" name="email-form" data-name="Email Form" method="get" class="form"
+                        data-wf-page-id="651f2c08c5bd81eb296c17fd"
+                        data-wf-element-id="502c5dca-196a-0aae-1f67-5b18c9a9023a">
+                        <div class="contact-method-payment">
+                            <div class="banks">
+                                <p>Aceptamos todos los métodos de pago en Marruecos</p>
+                                <div class="payments">
+                                    <img src="{{ asset('img/mar-banks.svg') }}" alt="Morocco banks">
+                                </div>
+                            </div>
+                            <div class="banks">
+                                <p>Aceptamos todos los métodos de pago en España</p>
+                                <div class="payments">
+                                    <img src="{{ asset('img/sp-banks.svg') }}" alt="Morocco banks">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="note-important">
+                            Rellena los siguientes datos para calcular el precio de tu seguro
+                        </div>
+                        <h6>1 - Apellido y Nombre :</h6>
+                        <input class="contacts-input white-style w-input" maxlength="256" name="name-2"
+                            data-name="Name 2" placeholder="Your name" type="text" id="name-2" required="" />
+                        <h6>2 - Seleccionar seguro :</h6>
+                        <input class="contacts-input white-style w-input" maxlength="256" name="email-2"
+                            data-name="Email 2" placeholder="Your email" type="email" id="email-2" required="" />
+                        <h6>3 -  Fecha de nacimiento :</h6>
+                        <div class="date">
+                            <input type="number" placeholder="Dia">
+                            <input type="number" placeholder="Mes">
+                            <input type="number" placeholder="Ano">
+                        </div>
+                        <h6>4 -  Sexo:</h6>
+                        <div class="check-type" style="margin-bottom: 20px">
+                            <div class="type man">
+                                <input type="radio">
+                                <label for="">Hombre</label>
+                            </div>
+                            <div class="type femal">
+                                <input type="radio">
+                                <label for="">Mujer</label>
+                            </div>
+                        </div>
+                        <input type="submit" data-wait="Please wait..."
+                            class="primary-button full-width-mobile w-button" value="Submit" />
+                    </form>
+                    <div class="success-message w-form-done">
+                        <div>Thank you! Your submission has been received!</div>
+                    </div>
+                    <div class="error-message w-form-fail">
+                        <div>Oops! Something went wrong while submitting the form.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-
+<!-- contact-components -->
+<div class="section contact-components">
+    <div class="base-container w-container">
+        <div class="contact-component-wrapper">
+            <div class="c-c-block">
+                <img src="/assets/images/svg/component-email.svg" alt="Email icone">
+                <div class="c-c-name">
+                    Email
+                </div>
+                <p>Direct all your queries to our dedicated customer support email.</p>
+                <a href="#">
+                    <div class="c-c-link">
+                        hello@nimble.llc
+                    </div>
+                </a>
+            </div>
+            <div class="c-c-block">
+                <img src="/assets/images/svg/component-chat.svg" alt="Email icone">
+                <div class="c-c-name">
+                    Email
+                </div>
+                <p>For real-time assistance, initiate a chat with our representative.</p>
+                <a href="#">
+                    <div class="c-c-link">Start new chat
+                    </div>
+                </a>
+            </div>
+            <div class="c-c-block">
+                <img src="/assets/images/svg/component-phone.svg" alt="Email icone">
+                <div class="c-c-name">
+                    Email
+                </div>
+                <p>Call us for immediate responses to your questions or concerns.</p>
+                <a href="#">
+                    <div class="c-c-link">+1 (555) 000-0000
+                    </div>
+                </a>
+            </div>
+            <div class="c-c-block">
+                <img src="/assets/images/svg/component-location.svg" alt="Email icone">
+                <div class="c-c-name">
+                    Email
+                </div>
+                <p>If you're in the area, feel free to drop by our office for assistance.</p>
+                <a href="#">
+                    <div class="c-c-link">123 Sample St, Los Angeles 2000 US
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+    <!-- Solutions -->
+    <div class="section solution-section without-bottom-spacing">
+        <div class="base-container w-container">
+            <div class="solution-heading">
+                <h2>Ofrecemos una gama de <span>soluciones para sus
+                        necesidades</span> legales en Espana.</h2>
+            </div>
+            <div class="solution-wrapper">
+                <div class="sol-block">
+                    <div class="sol-overlay"></div>
+                    <img src="/assets/images/solution-0.png" alt="Solutions Image">
+                    <div class="sol-content">
+                        <div class="sol-heading">Golden visa</div>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                </div>
+                <div class="sol-block">
+                    <div class="sol-overlay"></div>
+                    <img src="/assets/images/solution-1.png" alt="Solutions Image">
+                    <div class="sol-content">
+                        <div class="sol-heading">Golden visa</div>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                </div>
+                <div class="sol-block">
+                    <div class="sol-overlay"></div>
+                    <img src="/assets/images/solution-2.png" alt="Solutions Image">
+                    <div class="sol-content">
+                        <div class="sol-heading">Cuenta propia</div>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                </div>
+                <div class="sol-block">
+                    <div class="sol-overlay"></div>
+                    <img src="/assets/images/solution-3.png" alt="Solutions Image">
+                    <div class="sol-content">
+                        <div class="sol-heading">Visa de estudiante</div>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <x-sp.footer />
