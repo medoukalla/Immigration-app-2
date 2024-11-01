1be4d255d306caf5a59e0b919e94c433a4649c47
Mohamed Oukalla <medoukalla@gmail.com>
2024-11-01 11:12:16 +0000

    Fix testimonials issue

--- resources/views/components/sp/testimonials.blade.php
+++ resources/views/components/sp/testimonials.blade.php
@@ -14,18 +14,20 @@
     <div class="swiper mySwiper">
       <div class="swiper-wrapper">
         <!-- Caja de comentarios -->
-        @for ($i = 0; $i < count($testimonials); $i++)
+
+        @foreach ($testimonials as $testimonial)
+        
           <div class="swiper-slide">
               <div class="writer-info">
               <div class="writer-left">
                   <div class="writer-img">
-                  S
+                  {{ substr($testimonial->name, 0, 1) }}
                   </div>
                   <div class="writer-n-y">
                   <div class="writer-n">
-                      {{ $testimonials[$i]->name }}
+                      {{ $testimonial->name }}
                   </div>
-                  <div class="writer-y">{{ $testimonials[$i]->created_at->diffForHumans() }}</div>
+                  <div class="writer-y">{{ $testimonial->created_at->diffForHumans() }}</div>
                   </div>
               </div>
               <div class="writer-right">
@@ -36,10 +38,11 @@
               <img class="google-stars" src="assets/images/svg/stars.svg" alt="Estrellas de Google">
               </div>
               <p>
-                  {{ $testimonials[$i]->textimonial }}
+                  {{ $testimonial->textimonial }}
               </p>
           </div>
-        @endfor
+            
+        @endforeach
 
       </div>
       <div class="swiper-pagination"></div>
