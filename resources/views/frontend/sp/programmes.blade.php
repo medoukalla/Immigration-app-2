4ce1a00cc27a5f696dd24d1d7ab8c971856a835d
marwanBenz <benzsram@gmail.com>
2024-11-11 14:08:48 +0100

    Re-fixed programmes studios design

--- resources/views/frontend/sp/programmes.blade.php
+++ resources/views/frontend/sp/programmes.blade.php
@@ -5,7 +5,7 @@
 <div class="pages-banner blog">
     <div class="base-container w-container">
         <div class="min-hero-wrapper">
-            <h1>Arraigo familiar</h1>
+            <h1>Programas de estudio</h1>
             <p>Estamos aquí para guiarte en cada paso del camino. Nuestro equipo de expertos está dedicado a hacer que
                 tu viaje a un nuevo país sea lo más fluido y libre de estrés posible.</p>
             <div class="pages-path">
