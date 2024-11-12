c1e5c79d6a5b1eee08f7bc2b3b1ab9b4ed647577
Mohamed Oukalla <medoukalla@gmail.com>
2024-11-12 22:03:25 +0000

    Show testimonials in all pages

--- resources/views/frontend/sp/arraigo_familiar.blade.php
+++ resources/views/frontend/sp/arraigo_familiar.blade.php
@@ -182,7 +182,7 @@
 
 
     <!-- Feedback -->
-    {{-- <x-sp.testimonials :testimonials="$testimonials" /> --}}
+    <x-sp.testimonials :testimonials="$testimonials" />
 
     <!-- Solutions -->
     <x-sp.solutions />
--- resources/views/frontend/sp/nomada_digital.blade.php
+++ resources/views/frontend/sp/nomada_digital.blade.php
@@ -174,7 +174,7 @@
 
 
     <!-- Feedback -->
-    {{-- <x-sp.testimonials :testimonials="$testimonials" /> --}}
+    <x-sp.testimonials :testimonials="$testimonials" />
 
     <!-- Solutions -->
     <x-sp.solutions />
