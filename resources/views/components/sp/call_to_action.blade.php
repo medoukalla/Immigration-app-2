aacf9900ae6e3ceb8030eb108c10e1dd77a138e1
Mohamed Oukalla <medoukalla@gmail.com>
2024-11-12 21:47:17 +0000

    Change call to action content

--- resources/views/components/sp/call_to_action.blade.php
+++ resources/views/components/sp/call_to_action.blade.php
@@ -4,11 +4,10 @@
     <div class="base-container w-container">
         <div class="call-to-action-b-box">
             <div class="call-b-subtitle">Solicita una asesoría personalizada por sólo 50€</div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis delectus tenetur fugiat,
-                praesentium
-                facere magnam labore laborum officiis architecto eum!</p>
-            <div data-w-id="cc582f0b-75ba-d7bd-9fe3-5b2888ed64ea" class="buttons-wrapper"><a href="{{ route('frontend.sp.contact') }}"
-                    class="primary-button w-button light-button">Contact us</a></div>
+            <p>Por sólo 50€, le ofrecemos una asesoría completa y adaptada a su situación específica. Le orientaremos con claridad sobre los pasos y requisitos necesarios para avanzar en su trámite.</p>
+            <div data-w-id="cc582f0b-75ba-d7bd-9fe3-5b2888ed64ea" class="buttons-wrapper">
+                <a href="javascript:void(0);" class="primary-button w-button light-button openModalButton">Reservar Consulta</a>
+                </div>
         </div>
     </div>
 </div>
