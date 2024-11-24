5b55a326f60fac40936f6591187a955f6d0e69dd
Mohamed Oukalla <medoukalla@gmail.com>
2024-11-24 21:13:53 +0000

    Switch contact form in component

--- resources/views/frontend/sp/residencia_no_lucrativa.blade.php
+++ resources/views/frontend/sp/residencia_no_lucrativa.blade.php
@@ -68,33 +68,7 @@
                     </div>
                 </div>
                 <!-- Contact Form -->
-                <div class="contacts-2-form-wrapper">
-                    <div class="form-block-contacts w-form">
-                        <form id="email-form" name="email-form" data-name="Email Form" method="get" class="form"
-                            data-wf-page-id="651f2c08c5bd81eb296c17fd"
-                            data-wf-element-id="502c5dca-196a-0aae-1f67-5b18c9a9023a">
-                            <h3 class="form-heading">Contacta con nosotros</h3>
-                            <input class="contacts-input white-style w-input" maxlength="256" name="name-2"
-                                data-name="Name 2" placeholder="Your name" type="text" id="name-2" required="" />
-                            <input class="contacts-input white-style w-input" maxlength="256" name="email-2"
-                                data-name="Email 2" placeholder="Your email" type="email" id="email-2" required="" />
-                            <input class="contacts-input white-style w-input" maxlength="256" name="email-2"
-                                data-name="Email 2" placeholder="Your phone number" type="email" id="email-2"
-                                required="" />
-                            <textarea placeholder="Your text" maxlength="5000" id="field-2" name="field-2"
-                                data-name="Field 2" required="" class="contacts-textarea white-style w-input">
-                                </textarea>
-                            <input type="submit" data-wait="Please wait..."
-                                class="primary-button full-width-mobile w-button" value="Submit" />
-                        </form>
-                        <div class="success-message w-form-done">
-                            <div>Thank you! Your submission has been received!</div>
-                        </div>
-                        <div class="error-message w-form-fail">
-                            <div>Oops! Something went wrong while submitting the form.</div>
-                        </div>
-                    </div>
-                </div>
+                <x-sp.small-contact-us />
             </div>
         </div>
     </div>
