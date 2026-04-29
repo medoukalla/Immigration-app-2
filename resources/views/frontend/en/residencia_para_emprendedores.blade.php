3d68c3a8fe7f14b68eb41a3927f4f5de8665c087
marwanbenz <benzsram@gmail.com>
2026-04-29 14:46:45 +0100

    Refactor Entrepreneur Residence page: Remove redundant content, enhance layout, and add legal requirements section for clarity.

--- resources/views/frontend/en/residencia_para_emprendedores.blade.php
+++ resources/views/frontend/en/residencia_para_emprendedores.blade.php
@@ -49,18 +49,9 @@
             <div class="numbers-text white-style">Satisfaction</div>
             <div class="line home-white-left"></div>
         </div>
-        <div id="w-node-_221627f6-3a31-260f-8a97-6ec174e99882-296c1813" class="working-wrap">
-            <div class="numbers">+1.200</div>
-            <div class="numbers-text white-style">Happy Clients</div>
-            <div class="line home-white-left"></div>
-        </div>
     </div>
 </div>
 
-It
-represents new opportunities and the beginning of a new stage in life.
-
-<<<<<<< HEAD
 <!-- Services contact form -->
 <div class="section">
     <div class="base-container w-container">
@@ -80,79 +71,11 @@ represents new opportunities and the beginning of a new stage in life.
                         ensuring compliance with all legal requirements.
                     </p>
                 </div>
-=======
-If you have been navigating complex legal requirements and finding contradictory or unclear
-information, you are not alone.
-
-Many individuals face the same challenge when trying to
-understand immigration procedures.
-
-Instead of relying on unreliable sources or generic platforms, you receive clear and
-structured legal guidance tailored to your situation.
-
-Your case is carefully analyzed, and you are provided with a clear strategy and practical
-solutions to move forward with confidence.
-
-Your next step in Spain starts here.
-            </p>
-            <div class="section-rootedness-grid">
-                <div class="left-rootedness">
-                    <div class="rootedness first-rootedness">
-                        <div class="number">1</div>
-                        <div class="number-title">
-                            Expert Legal Guidance
-                        </div>
-                        <p>
-                            You are guided step by step through every stage of your immigration process. Complex legal
-                            procedures are explained clearly, so you always know what to expect and how to proceed.
-                        </p>
-                    </div>
-                    <div class="rootedness second-rootedness">
-                        <div class="number">2</div>
-                        <div class="number-title">
-                            Personalized Solutions
-                        </div>
-                        <p>
-                            Every case is unique. Your situation is carefully analyzed to design a legal strategy tailored
-                            to your specific needs and goals, because behind every application there is a personal story.
-                        </p>
-                    </div>
-                </div>
-                <div class="midle-rootedness-img">
-                    <img src="{{ asset('assets/images/why-choose-us.png') }}" alt="" />
-                </div>
-                <div class="right-rootedness">
-                    <div class="rootedness third-rootedness">
-                        <div class="number">3</div>
-                        <div class="number-title">
-                            Proven Track Record
-                        </div>
-                        <p>
-                            With hundreds of satisfied clients, Immiworld has built a strong track record of achieving
-                            successful outcomes in immigration, nationality and residency matters.
-                        </p>
-                    </div>
-                    <div class="rootedness forth-rootedness">
-                        <div class="number">4</div>
-                        <div class="number-title">
-                            A Team You Can Trust
-                        </div>
-                        <p>
-                            You receive close and continuous support throughout the entire process, ensuring
-                            professionalism, transparency and confidence at every stage.
-                        </p>
-                    </div>
-                </div>
-                <div class="midle-rootedness-img-mobile">
-                    <img src="{{ asset('assets/images/why-choose-us.png') }}" alt="" />
-                </div>
->>>>>>> a7eb6ca7 (Refactor Entrepreneur Residence page: Update content, improve layout, and add legal requirements section. Create new duplicate page for "Arraigo de Segunda Oportunidad" with structured content and necessary documents.)
             </div>
             <!-- Contact Form -->
             @livewire('contact-english', ['service' => 'Entrepreneurs Residency'])
         </div>
     </div>
-<<<<<<< HEAD
 </div>
 <x-en.trust_immiworld />
 
@@ -193,218 +116,14 @@ Your next step in Spain starts here.
                     <p> Depending on the type of activity, additional documents may be required, such as specific
                         licenses or professional accreditations.
                     </p>
-=======
-</section>
-<section class="what-is-need-section">
-    <div class="ws-wrapper">
-        <div class="base-container w-container">
-            <div class="what-is-need">
-                <div class="win-content">
-                    <div class="win-heading">Requisitos Legales</div>
-                    <p>
-                        Legal Requirements
-                    </p>
-                    <div class="win-accordion">
-                        <div class="win-accordion-wrapper">
-                            <div class="win-accordion-item">
-                                <div class="win-accordion-header">
-                                    <div class="win-number">1</div>
-                                    <span>Valid Passport
-                                    </span>
-                                </div>
-                                <div class="win-accordion-body">
-                                    <div class="win-accordion-content">
-                                        You must provide a valid passport, including all pages.
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="win-accordion-item">
-                                <div class="win-accordion-header">
-                                    <div class="win-number">2</div>
-                                    <span>Business Plan
-                                    </span>
-                                </div>
-                                <div class="win-accordion-body">
-                                    <div class="win-accordion-content">
-                                        You must submit a detailed business plan demonstrating the viability, innovation and
-potential economic impact of your project in Spain.
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="win-accordion-item">
-                                <div class="win-accordion-header">
-                                    <div class="win-number">3</div>
-                                    <span>Proof of Financial Means</span>
-                                </div>
-                                <div class="win-accordion-body">
-                                    <div class="win-accordion-content">
-                                        You must demonstrate sufficient financial resources to establish and sustain your business,
-as well as to cover your personal living expenses.
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="win-accordion-item">
-                                <div class="win-accordion-header">
-                                    <div class="win-number">4</div>
-                                    <span>Additional Documentation</span>
-                                </div>
-                                <div class="win-accordion-body no-border">
-                                    <div class="win-accordion-content">
-                                        Depending on the type of activity, additional documents may be required, such as licenses,
-permits or professional accreditations.
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <a href="#" class="primary-button w-button" style="width: 50%">Submit</a>
-                </div>
-                <div class="empty-div">
-                    <img style="width: 100%; height: 100%" src="../../../../../public/assets/images/docs-img-right.png"
-                        alt="" />
->>>>>>> a7eb6ca7 (Refactor Entrepreneur Residence page: Update content, improve layout, and add legal requirements section. Create new duplicate page for "Arraigo de Segunda Oportunidad" with structured content and necessary documents.)
                 </div>
             </div>
         </div>
     </div>
-<<<<<<< HEAD
 </div>
 
 
 <x-en.call_to_action />
-=======
-    <div class="win-img">
-        <img style="width: 100%; height: 100%" src="{{ asset('assets/images/docs-img-right.png') }}" alt="" />
-    </div>
-</section>
-<section class="what-is-need-section left">
-    <div class="ws-wrapper">
-        <div class="base-container w-container">
-            <div class="what-is-need reverse">
-                <div class="win-content right">
-                    <div class="win-heading">Requisitos Requeridos</div>
-                    <p>
-                        Para garantizar el éxito de su solicitud de arraigo
-                        laboral, seguimos un proceso estructurado de asesoría y
-                        gestión documental. A continuación, detallamos las fases
-                        clave y los requisitos necesarios para completar su
-                        trámite con total seguridad jurídica.
-                    </p>
-                    <div class="win-accordion">
-                        <div class="win-accordion-wrapper">
-                            <div class="win-accordion-item">
-                                <div class="win-accordion-header">
-                                    <div class="win-number">1</div>
-                                    <span>Evaluación Inicial</span>
-                                </div>
-                                <div class="win-accordion-body">
-                                    <div class="win-accordion-content">
-                                        Realizamos una consulta personalizada
-                                        para evaluar a fondo su situación y
-                                        confirmar que cumple con los requisitos
-                                        generales. Además, le proporcionamos un
-                                        resumen claro del proceso y de la
-                                        documentación necesaria.
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="win-accordion-item">
-                                <div class="win-accordion-header">
-                                    <div class="win-number">2</div>
-                                    <span>Revisión y Preparación de
-                                        Documentación</span>
-                                </div>
-                                <div class="win-accordion-body">
-                                    <div class="win-accordion-content">
-                                        Reunimos y revisamos toda la
-                                        documentación requerida, verificando su
-                                        conformidad con las normativas vigentes.
-                                        Nuestro equipo organiza y prepara los
-                                        documentos de manera exhaustiva para
-                                        evitar retrasos en la solicitud.
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="win-accordion-item">
-                                <div class="win-accordion-header">
-                                    <div class="win-number">3</div>
-                                    <span>Gestión Integral del Proceso</span>
-                                </div>
-                                <div class="win-accordion-body">
-                                    <div class="win-accordion-content">
-                                        Nos ocupamos de gestionar todo el
-                                        proceso desde la preparación hasta la
-                                        presentación de su solicitud. También
-                                        asistimos en cualquier requisito
-                                        adicional necesario para asegurar el
-                                        éxito del trámite.
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="win-accordion-item">
-                                <div class="win-accordion-header">
-                                    <div class="win-number">4</div>
-                                    <span>Asesoría y Seguimiento Posterior</span>
-                                </div>
-                                <div class="win-accordion-body">
-                                    <div class="win-accordion-content">
-                                        Realizamos un seguimiento continuo de la
-                                        solicitud y le mantenemos informado de
-                                        cada avance importante. En caso de
-                                        resolución favorable, le orientamos en
-                                        los pasos finales; si es desfavorable,
-                                        le asesoramos sobre las opciones de
-                                        recurso.
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="win-accordion-item">
-                                <div class="win-accordion-header">
-                                    <div class="win-number">5</div>
-                                    <span>Acreditación de la Relación
-                                        Laboral</span>
-                                </div>
-                                <div class="win-accordion-body">
-                                    <div class="win-accordion-content">
-                                        Deberás demostrar la existencia de
-                                        relaciones laborales cuya duración no
-                                        sea inferior a seis meses, mediante
-                                        resolución judicial o acta de Inspección
-                                        de Trabajo.
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="win-accordion-item">
-                                <div class="win-accordion-header">
-                                    <div class="win-number">6</div>
-                                    <span>Documentación de Identidad
-                                        Completa</span>
-                                </div>
-                                <div class="win-accordion-body no-border">
-                                    <div class="win-accordion-content">
-                                        Es obligatorio aportar la copia íntegra
-                                        del pasaporte. Se deben incluir todas
-                                        las páginas para verificar los periodos
-                                        de permanencia y entradas o salidas.
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <a href="#" class="primary-button w-button" style="width: 50%">Submit</a>
-                </div>
-                <div class="empty-div left">
-                    <img style="width: 100%; height: 100%" src="../../../../../public/assets/images/docs-img-right.png"
-                        alt="" />
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="win-img">
-        <img style="width: 100%; height: 100%" src="{{ asset('assets/images/docs-img-right.png') }}" alt="" />
-    </div>
-</section>
->>>>>>> a7eb6ca7 (Refactor Entrepreneur Residence page: Update content, improve layout, and add legal requirements section. Create new duplicate page for "Arraigo de Segunda Oportunidad" with structured content and necessary documents.)
 
 
 <!-- Necessary documents -->
